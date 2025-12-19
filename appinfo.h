405c15435d715ccea0b30842d48e59514ee6abf7
潘翾寰 <3162524860@qq.com>
2025-12-19 17:54:26 +0800

    chore: fill in student info

--- appinfo.h
+++ appinfo.h
@@ -6,9 +6,7 @@ namespace AppInfo {
 
 inline const QString kAppTitle = QStringLiteral("医院诊疗系统");
 
-// TODO: 按实际信息填写，用于界面显示（实验要求）。
-inline const QString kStudentId = QStringLiteral("2025xxxxxx");
-inline const QString kStudentName = QStringLiteral("你的姓名");
+inline const QString kStudentId = QStringLiteral("2023414290320");
+inline const QString kStudentName = QStringLiteral("潘翾寰");
 
 }
-
